35d9ac1d04280f4346ce8f1a23bbd905b6d2fec4
jaerim <jaerim1102@naver.com>
2024-12-06 04:04:50 +0900

    스킬 환경 구성

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import { Link } from "react-scroll";
 import "./styles/App.css";
 import Home from "./sections/Home";
 import Profile from "./sections/Profile";
+import Skills from "./sections/Skills";
 
 const App = () => {
   return (
     <div>
       <Home id="section1" title="Home" />
       <Profile id="section2" title="Profile" />
-      {/* <Section id="section2" title="About" />
-      <Section id="section3" title="Projects" />
-      <Section id="section4" title="Contact" /> */}
+      <Skills id="section3" title="Skills" />
     </div>
   );
 };
---
+++ src/sections/Skills.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "../styles/skills.css";
+
+const Skills = () => {
+  return (
+    <div className="skill-component">
+      <h1>Skills</h1>
+    </div>
+  );
+};
+
+export default Skills;
