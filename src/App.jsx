fea6764d6454a4aaa5e2b91b6f08013f2d50847a
jaerim <jaerim1102@naver.com>
2024-12-05 22:57:38 +0900

    home 섹션 마크업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import React from "react";
 import { Link } from "react-scroll";
+import "./styles/App.css";
+import Home from "./sections/Home";
 
 const App = () => {
   return (
     <div>
-      <nav>
-        <Link to="section1" smooth={true}>
-          Home
-        </Link>
-        <Link to="section2" smooth={true}>
-          About
-        </Link>
-        <Link to="section3" smooth={true}>
-          Projects
-        </Link>
-        <Link to="section4" smooth={true}>
-          Contact
-        </Link>
-      </nav>
-      <Section id="section1" title="Home" />
-      <Section id="section2" title="About" />
+      <Home id="section1" title="Home" />
+      {/* <Section id="section2" title="About" />
       <Section id="section3" title="Projects" />
-      <Section id="section4" title="Contact" />
+      <Section id="section4" title="Contact" /> */}
     </div>
   );
 };
---
+++ src/sections/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-title">
+      <div className="home-serve-title">
+        <span>2024</span>
+        <span>PORTFOLIO</span>
+        <span>S2</span>
+      </div>
+      <h1>
+        <span>I'M JAERIM KIM</span>
+        <span>A GROWING</span>
+        <span>FRONT-END</span>
+        <span>DEVELOPER</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
