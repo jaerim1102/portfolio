2dd19dd83bb47367d1452b8a1856839bc09f554e
jaerim <jaerim1102@naver.com>
2024-12-05 20:19:43 +0900

    환경 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Link } from "react-scroll";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <nav>
+        <Link to="section1" smooth={true}>
+          Home
+        </Link>
+        <Link to="section2" smooth={true}>
+          About
+        </Link>
+        <Link to="section3" smooth={true}>
+          Projects
+        </Link>
+        <Link to="section4" smooth={true}>
+          Contact
+        </Link>
+      </nav>
+      <Section id="section1" title="Home" />
+      <Section id="section2" title="About" />
+      <Section id="section3" title="Projects" />
+      <Section id="section4" title="Contact" />
+    </div>
+  );
+};
 
-export default App
+export default App;
