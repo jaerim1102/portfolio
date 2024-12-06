a1e8cf9bea528282a35cf2a24ce3dff262a363c2
jaerim <jaerim1102@naver.com>
2024-12-06 10:58:42 +0900

    가로 그래프

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import pb from "../lib/pocketbase";
+import "../styles/skills.css";
 
 const Skills = () => {
   const [skills, setSkills] = useState([]);
@@ -19,16 +20,32 @@ const Skills = () => {
 
   return (
     <div>
-      <h2>My Skills</h2>
-      <div>
+      <table>
         {skills.map((skill) => (
-          <div key={skill.id}>
-            {/* skill_img가 파일 필드일 경우 */}
-            <img src={pb.getFileUrl(skill, skill.skill_img)} alt={skill.name} />
-            <p>{skill.name}</p>
-          </div>
+          <tr key={skill.id}>
+            <td>
+              <img
+                src={pb.getFileUrl(skill, skill.skill_img)}
+                alt={skill.skill_name}
+                className="skill-img"
+              />
+            </td>
+            <td>{skill.skill_name}</td>
+            <td>{skill.skill_explanation}</td>
+            <td>
+              <div className="progress-bar">
+                <div
+                  className="progress-bar-fill"
+                  style={{ width: `${skill.skill_proficiency}%` }}
+                ></div>
+              </div>
+              <span className="proficiency-text">
+                {skill.skill_proficiency}%
+              </span>
+            </td>
+          </tr>
         ))}
-      </div>
+      </table>
     </div>
   );
 };
