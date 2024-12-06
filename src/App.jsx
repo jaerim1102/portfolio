5d976a6794637f51c1b17aae9a12403e2d787551
jaerim <jaerim1102@naver.com>
2024-12-06 13:44:05 +0900

    프로젝트 환경구성

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import "./styles/App.css";
 import Home from "./sections/Home";
 import Profile from "./sections/Profile";
 import Skills from "./sections/Skills";
+import Project from "./sections/Project";
 
 const App = () => {
   return (
-    <div>
+    <div className="main">
       <Home id="section1" title="Home" />
       <Profile id="section2" title="Profile" />
-      <Skills id="section3" title="Skills" />
+      <Project id="section3" title="Project" />
+      <Skills id="section4" title="Skills" />
     </div>
   );
 };
---
+++ src/sections/Project.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import pb from "../lib/pocketbase";
+import "../styles/project.css";
+
+const Project = () => {
+  return (
+    <div className="project-container">
+      <h2>Project</h2>
+    </div>
+  );
+};
+
+export default Project;
