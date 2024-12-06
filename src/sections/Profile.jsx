094f36b955c0608a8151cd08c1a95cedcfffe497
jaerim <jaerim1102@naver.com>
2024-12-06 12:16:23 +0900

    스타일링 수정

--- src/sections/Profile.jsx
+++ src/sections/Profile.jsx
@@ -47,12 +47,14 @@ const Profile = () => {
               <span>2021 교육부 서포터즈 </span>
               <span>2023 커뮤니케이션 디자인 국제 공모전</span>
               <span>멋쟁이사자처럼 프론트엔드 스쿨 10기</span>
+              <span>2024 계원예술대학교 조형제</span>
             </div>
             <div className="awards-info">
               <p>시각디자인 부문 입선</p>
               <p>영상 편집 총괄</p>
               <p>동상</p>
               <p>수료</p>
+              <p>최우수작</p>
             </div>
           </div>
         </div>
