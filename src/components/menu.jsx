127ba6e7a586eccb9a87227f26663b5a626046ae
jaerim <jaerim1102@naver.com>
2024-12-07 19:33:04 +0900

    메뉴 마크업

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,15 +1,31 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
+import { Link } from "react-scroll";
 import "../styles/menu.css";
 
 const Menu = () => {
   return (
     <div className="menu">
       <ul>
-        <li>Home</li>
-        <li>Profile</li>
-        <li>Project</li>
-        <li>Skill</li>
+        <li>
+          <Link to="section1" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="section2" smooth={true} duration={500}>
+            Profile
+          </Link>
+        </li>
+        <li>
+          <Link to="section3" smooth={true} duration={500}>
+            Project
+          </Link>
+        </li>
+        <li>
+          <Link to="section4" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
       </ul>
     </div>
   );
