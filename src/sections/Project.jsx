e99c96d5dcab503e915119bc2bf513a337542a5a
jaerim <jaerim1102@naver.com>
2024-12-10 02:56:28 +0900

    프로젝트 스타일링 점검

--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import pb from "../lib/pocketbase"; // PocketBase 인스턴스
+import "../styles/project.css";
 
 const Project = () => {
   const [projects, setProjects] = useState([]);
@@ -18,58 +19,60 @@ const Project = () => {
   }, []);
 
   return (
-    <div className="project-list">
-      {projects.map((project) => (
-        <div key={project.id} className="project-card">
-          <img
-            src={pb.getFileUrl(project, project.project_img)}
-            alt={project.project_name}
-            className="project-image"
-          />
-          <h2>{project.project_name}</h2>
-          <p>
-            <strong>Project Time:</strong> {project.Project_time}
-          </p>
-          <p>
-            <strong>Team:</strong> {project.project_team}
-          </p>
-          <p>
-            <strong>Explanation:</strong> {project.project_explanation}
-          </p>
-          <p>
-            <strong>My Role:</strong> {project.project_mine}
-          </p>
-          <p>
-            <strong>Additional Info:</strong> {project.project_etc}
-          </p>
-          <p>
-            <strong>URL:</strong>{" "}
-            <a
-              href={project.project_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {project.project_url}
-            </a>
-          </p>
-          <p>
-            <strong>GitHub:</strong>{" "}
-            <a
-              href={project.project_github}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {project.project_github}
-            </a>
-          </p>
-          <p>
-            <strong>Stack:</strong>{" "}
-            {project.project_stack && project.project_stack.length > 0
-              ? project.project_stack.join(", ")
-              : "No stack provided"}
-          </p>
-        </div>
-      ))}
+    <div className="project-container">
+      <h2 className="project-h2">Projects</h2>
+      <div className="project-content">
+        {projects.map((project) => (
+          <div key={project.id} className="project-card">
+            <p>{project.Project_time}</p>
+            <h2>{project.project_name}</h2>
+
+            <img
+              src={pb.getFileUrl(project, project.project_img)}
+              alt={project.project_name}
+              className="project-image"
+            />
+            <p>
+              <strong>Team:</strong> {project.project_team}
+            </p>
+            <p>
+              <strong>Explanation:</strong> {project.project_explanation}
+            </p>
+            <p>
+              <strong>My Role:</strong> {project.project_mine}
+            </p>
+            <p>
+              <strong>Additional Info:</strong> {project.project_etc}
+            </p>
+            <p>
+              <strong>URL:</strong>{" "}
+              <a
+                href={project.project_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {project.project_url}
+              </a>
+            </p>
+            <p>
+              <strong>GitHub:</strong>{" "}
+              <a
+                href={project.project_github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {project.project_github}
+              </a>
+            </p>
+            <p>
+              <strong>Stack:</strong>{" "}
+              {project.project_stack && project.project_stack.length > 0
+                ? project.project_stack.join(", ")
+                : "No stack provided"}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
