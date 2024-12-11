2ecba7b19b8d2737d6ba4fab05c1537afbdc2ff0
jaerim <jaerim1102@naver.com>
2024-12-11 09:38:06 +0900

    자기소개서 저장 기능 구현

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,9 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/download.css";
 
 const DownloadButton = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/assets/jaerim.pdf"; // 다운로드할 파일 경로
+    link.download = "김재림_자기소개서.pdf"; // 다운로드될 파일 이름
+    link.click();
+  };
+
   return (
-    <button className="download-button">
+    <button className="download-button" onClick={handleDownload}>
       자기소개서 <img src="/assets/img/download.svg" alt="다운로드" />
     </button>
   );
